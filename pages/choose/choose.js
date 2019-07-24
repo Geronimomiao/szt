3e48dbed12e8fe05464a3cea78b4d6679ae81b3a
wsm <1030057982@qq.com>
2019-07-24 15:11:23 +0800

    嘤嘤嘤
    目录结构设计完毕

---
+++ pages/choose/choose.js
@@ -0,0 +1,28 @@
+const { routerMap } = require('../../utils/util.js')
+
+Page({
+  data: {
+    dest: '',
+    course_name: ''
+  },
+
+  
+  onLoad: function (options) {
+    this.setData({
+      dest: options.dest
+    })
+  },
+
+  go(e) {
+    this.setData({
+      course_name: e.currentTarget.dataset.cur,
+    })
+    this.goForDest()
+  },
+
+  goForDest() {
+    console.log(routerMap[this.data.dest])
+    const destUrl = routerMap[this.data.dest]
+  }
+
+})
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,10 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
-
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+const routerMap = {
+  'courseware': '/pages/courseware/courseware',
+  'courseroom': '/pages/courseroom/courseroom',
+  'createcourse': '/pages/createcourse/createcourse',
+  'video': '/pages/video/video',
+} 
 
 module.exports = {
-  formatTime: formatTime
+  routerMap
 }
